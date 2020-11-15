acf9cf4a8b48a3a482cda2eecf36a6c7651d1003
unknown <18cmorabito@gmail.com>
2020-11-15 17:58:45 -0500

    Made the hourly and weekly weather functional, created icon sorting.

--- public/javascripts/hourlyWeather.js
+++ public/javascripts/hourlyWeather.js
@@ -1,8 +1,122 @@
+const dayIconList = {
+    200: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    201: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    202: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    210: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    211: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    212: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    221: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    230: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    231: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    232: '/images/day-icons/ios11-weather-thunderstorm-icon.png',
+    300: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    301: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    302: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    310: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    311: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    312: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    313: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    314: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    321: '/images/day-icons/ios11-weather-drizzle-icon.png',
+    500: '/images/day-icons/ios11-weather-rain-icon.png',
+    501: '/images/day-icons/ios11-weather-rain-icon.png',
+    502: '/images/day-icons/ios11-weather-heavy-rain-icon.png',
+    503: '/images/day-icons/ios11-weather-heavy-rain-icon.png',
+    504: '/images/day-icons/ios11-weather-heavy-rain-icon.png',
+    511: '/images/day-icons/ios11-weather-hail-icon.png',
+    520: '/images/day-icons/ios11-weather-rain-icon.png',
+    521: '/images/day-icons/ios11-weather-rain-icon.png',
+    522: '/images/day-icons/ios11-weather-heavy-rain-icon.png',
+    531: '/images/day-icons/ios11-weather-rain-icon.png',
+    600: '/images/day-icons/ios11-weather-scattered-snow-icon.png',
+    601: '/images/day-icons/ios11-weather-snow-icon.png',
+    602: '/images/day-icons/ios11-weather-snow-sleet-icon.png',
+    611: '/images/day-icons/ios11-weather-snow-sleet-icon.png',
+    612: '/images/day-icons/ios11-weather-snow-sleet-icon.png',
+    613: '/images/day-icons/ios11-weather-snow-sleet-icon.png',
+    615: '/images/day-icons/ios11-weather-hail-icon.png',
+    616: '/images/day-icons/ios11-weather-hail-icon.png',
+    620: '/images/day-icons/ios11-weather-snow-icon.png',
+    621: '/images/day-icons/ios11-weather-snow-icon.png',
+    622: '/images/day-icons/ios11-weather-snow-sleet-icon.png',
+    701: '/images/day-icons/ios11-weather-haze-icon.png',
+    711: '/images/day-icons/ios11-weather-smoke-icon.png',
+    721: '/images/day-icons/ios11-weather-haze-icon.png',
+    731: '/images/day-icons/ios11-weather-dust-icon.png',
+    741: '/images/day-icons/ios11-weather-fog-icon.png',
+    751: '/images/day-icons/ios11-weather-dust-icon.png',
+    761: '/images/day-icons/ios11-weather-dust-icon.png',
+    762: '/images/no-report-icon.png',
+    771: '/images/no-report-icon.png',
+    781: '/images/day-icons/ios11-weather-tornado-icon.png',
+    800: '/images/day-icons/ios11-weather-sunny-icon.png',
+    801: '/images/day-icons/ios11-weather-partly-sunny-icon.png',
+    802: '/images/day-icons/ios11-weather-partly-sunny-icon.png',
+    803: '/images/day-icons/ios11-weather-cloudy-icon.png',
+    804: '/images/day-icons/ios11-weather-cloudy-icon.png'
+};
+const nightIconList = {
+    200: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    201: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    202: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    210: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    211: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    212: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    221: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    230: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    231: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    232: '/images/night-icons/ios11-weather-thunderstorm-icon.png',
+    300: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    301: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    302: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    310: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    311: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    312: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    313: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    314: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    321: '/images/night-icons/ios11-weather-drizzle-icon.png',
+    500: '/images/night-icons/ios11-weather-rain-icon.png',
+    501: '/images/night-icons/ios11-weather-rain-icon.png',
+    502: '/images/night-icons/ios11-weather-heavy-rain-icon.png',
+    503: '/images/night-icons/ios11-weather-heavy-rain-icon.png',
+    504: '/images/night-icons/ios11-weather-heavy-rain-icon.png',
+    511: '/images/night-icons/ios11-weather-hail-icon.png',
+    520: '/images/night-icons/ios11-weather-rain-icon.png',
+    521: '/images/night-icons/ios11-weather-rain-icon.png',
+    522: '/images/night-icons/ios11-weather-heavy-rain-icon.png',
+    531: '/images/night-icons/ios11-weather-rain-icon.png',
+    600: '/images/night-icons/ios11-weather-scattered-snow-icon.png',
+    601: '/images/night-icons/ios11-weather-snow-icon.png',
+    602: '/images/night-icons/ios11-weather-snow-sleet-icon.png',
+    611: '/images/night-icons/ios11-weather-snow-sleet-icon.png',
+    612: '/images/night-icons/ios11-weather-snow-sleet-icon.png',
+    613: '/images/night-icons/ios11-weather-snow-sleet-icon.png',
+    615: '/images/night-icons/ios11-weather-hail-icon.png',
+    616: '/images/night-icons/ios11-weather-hail-icon.png',
+    620: '/images/night-icons/ios11-weather-snow-icon.png',
+    621: '/images/night-icons/ios11-weather-snow-icon.png',
+    622: '/images/night-icons/ios11-weather-snow-sleet-icon.png',
+    701: '/images/night-icons/ios11-weather-haze-icon.png',
+    711: '/images/night-icons/ios11-weather-smoke-icon.png',
+    721: '/images/night-icons/ios11-weather-haze-icon.png',
+    731: '/images/night-icons/ios11-weather-dust-icon.png',
+    741: '/images/night-icons/ios11-weather-fog-icon.png',
+    751: '/images/night-icons/ios11-weather-dust-icon.png',
+    761: '/images/night-icons/ios11-weather-dust-icon.png',
+    762: '/images/no-report-icon.png',
+    771: '/images/no-report-icon.png',
+    781: '/images/night-icons/ios11-weather-tornado-icon.png',
+    800: '/images/night-icons/ios11-weather-clear-night-icon.png',
+    801: '/images/night-icons/ios11-weather-partly-cloudy-night-icon.png',
+    802: '/images/night-icons/ios11-weather-partly-cloudy-night-icon.png',
+    803: '/images/night-icons/ios11-weather-cloudy-icon.png',
+    804: '/images/night-icons/ios11-weather-cloudy-icon.png'
+};
+
 $.ajax({
     method: 'GET',
     url: 'https://api.openweathermap.org/data/2.5/weather?q=state college&appid=c0c35b925bbddaaf1dca134adf31f13a',
     success: function(data) {
-        console.log(data);
         runAPI(data);
     }
 });
@@ -10,18 +124,17 @@ $.ajax({
 function runAPI(obj) {
     var lat = obj.coord.lat;
     var lon = obj.coord.lon;
-    console.log(lat + " " + lon);
+    var cityName = obj.name;
     $.ajax({
         method: 'GET',
         url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=daily,minutely,alerts&units=imperial&appid=c0c35b925bbddaaf1dca134adf31f13a',
         success: function(data) {
-            populateWeather(data);
+            populateWeather(data, cityName);
         }
     });
 }
 
-function populateWeather(obj) {
-    console.log(obj.current.weather);
+function populateWeather(obj, cityName) {
     for (var i = 0; i < 6; i++) {
         var hId = "#hour" + (i + 1);
         var iId = "#icon" + (i + 1);
@@ -29,12 +142,11 @@ function populateWeather(obj) {
         var aChild = $(hId)[0].children[0].children;
         var nData = [];
         var arrayTracker = 0;
-        console.log(hour);
 
-        $(iId).attr("src", changeIcon(hour.weather[0].id));
+        $(iId).attr("src", changeIcon(hour.weather[0].id, hour.weather[0].icon.substring(2)));
 
         nData.push(convertToDate(hour.dt).toLocaleTimeString([], { hour: 'numeric', minute: 'numeric', hour12: true }));
-        nData.push("State College, PA");
+        nData.push(cityName);
         nData.push(Math.round(hour.temp) + "&deg;F");
         nData.push(hour.weather[0].description);
         nData.push("Humidity: " + hour.humidity + "%");
@@ -54,20 +166,10 @@ function convertToDate(n) {
     return new Date(time);
 }
 
-function changeIcon(id) {
-    if (id == 800) {
-        return "images/ios11-weather-sunny-icon.png";
-    } else if (id == 801 || id == 802) {
-        return "/images/ios11-weather-partly-sunny-icon.png";
-    } else if (id == 803 || id == 804) {
-        return "/images/ios11-weather-cloudy-icon.png"
-    } else if (id > 599 && id < 700) {
-        return "/images/ios11-weather-snow-icon.png";
-    } else if (id > 499 && id < 600) {
-        return "/images/ios11-weather-rain-icon.png";
-    } else if (id > 299 && id < 400) {
-        return "/images/ios11-weather-drizzle-icon.png";
-    } else {
-        return "/images/ios11-weather-thunderstorm-icon.png";
+function changeIcon(id, time) {
+    if (time == "d") {
+        return dayIconList[id];
+    } else if (time == "n") {
+        return nightIconList[id];
     }
 }
