82aa746da6286587775d675543065e72ddeb5e5e
unknown <18cmorabito@gmail.com>
2020-11-18 11:04:46 -0500

    Touched up javascript

--- public/javascripts/currentWeather.js
+++ public/javascripts/currentWeather.js
@@ -1,97 +1,99 @@
 // Ensure the JS code is loaded before running
 $(document).ready(function() {
-  let loc1 = sessionStorage.getItem("lat");
-  let loc2 = sessionStorage.getItem("lon");
-  let loc3 = sessionStorage.getItem("zipCode");
-  alert('Latitude: ' + loc1 + " Lon: " + loc2 + " zip: " + loc3);
+    let loc1 = sessionStorage.getItem("lat");
+    let loc2 = sessionStorage.getItem("lon");
+    let loc3 = sessionStorage.getItem("zipCode");
+    var name = sessionStorage.getItem("cityName");
+    alert('Latitude: ' + loc1 + " Lon: " + loc2 + " zip: " + loc3 + " city: " + name);
 });
 
 // Want to get the text input
 const nameInput = document.getElementById("input");
 document
-  .querySelector("form.search-form")
-  //   On submit event on the form
-  .addEventListener("submit", function (e) {
-    //prevent the normal submission of the form
-    e.preventDefault();
-    // Retrieving the API with the unique API key
-    $.getJSON(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-        nameInput.value +
-        ",us&units=imperial&appid=c0c35b925bbddaaf1dca134adf31f13a",
-      function (data) {
-        console.log(data);
-        console.log("User entered: " +nameInput.value);
-        // selecting the icon form the API
-        let icon =
-          "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        $(".icon").attr("src", icon);
+    .querySelector("form.search-form")
+    //   On submit event on the form
+    .addEventListener("submit", function(e) {
+        //prevent the normal submission of the form
+        e.preventDefault();
+        // Retrieving the API with the unique API key
+        $.getJSON(
+            "http://api.openweathermap.org/data/2.5/weather?q=" +
+            nameInput.value +
+            ",us&units=imperial&appid=c0c35b925bbddaaf1dca134adf31f13a",
+            function(data) {
+                console.log(data);
+                console.log("User entered: " + nameInput.value);
+                // selecting the icon form the API
+                let icon =
+                    "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+                $(".icon").attr("src", icon);
 
-        let temp = Math.floor(data.main.temp);
-        $(".temp").append(temp + "&deg;");
+                let temp = Math.floor(data.main.temp);
+                $(".temp").append(temp + "&deg;");
 
-        let weather = data.weather[0].main;
-        $(".condition").append(weather);
-        // Change the background image and color depending on the weather condition.
-        if (weather === "Clear") {
-          document.body.style.backgroundImage = "url('images/clearSky.jpg')";
-          document.getElementById("container").style.background = "#6691BB";
-        } else if (weather === "Rain") {
-          document.body.style.backgroundImage = "url('images/rain.jpg')";
-          document.getElementById("container").style.background =
-            "linear-gradient(180deg, rgba(127, 123, 130, 1) 0%, #3c3c3c 100%";
-        } else if (weather === "Clouds") {
-          document.body.style.backgroundImage = "url('images/cloudy.jpg')";
-          document.getElementById("container").style.background = "#465367";
-        } else if (weather === "Thunderstorm") {
-          document.body.style.backgroundImage = "url('images/thunder.jpg')";
-          document.getElementById("container").style.background = "#78777F";
-        } else if (weather === "Snow") {
-          document.body.style.backgroundImage = "url('images/snow.jpg')";
-          document.getElementById("container").style.background = "#759AAC";
-        } else if (weather === "Fog" || weather === "Mist") {
-          document.body.style.backgroundImage = "url('images/fog.jpg')";
-          document.getElementById("container").style.background = "#78777F";
-        }
+                let weather = data.weather[0].main;
+                $(".condition").append(weather);
+                // Change the background image and color depending on the weather condition.
+                if (weather === "Clear") {
+                    document.body.style.backgroundImage = "url('images/clearSky.jpg')";
+                    document.getElementById("container").style.background = "#6691BB";
+                } else if (weather === "Rain") {
+                    document.body.style.backgroundImage = "url('images/rain.jpg')";
+                    document.getElementById("container").style.background =
+                        "linear-gradient(180deg, rgba(127, 123, 130, 1) 0%, #3c3c3c 100%";
+                } else if (weather === "Clouds") {
+                    document.body.style.backgroundImage = "url('images/cloudy.jpg')";
+                    document.getElementById("container").style.background = "#465367";
+                } else if (weather === "Thunderstorm") {
+                    document.body.style.backgroundImage = "url('images/thunder.jpg')";
+                    document.getElementById("container").style.background = "#78777F";
+                } else if (weather === "Snow") {
+                    document.body.style.backgroundImage = "url('images/snow.jpg')";
+                    document.getElementById("container").style.background = "#759AAC";
+                } else if (weather === "Fog" || weather === "Mist") {
+                    document.body.style.backgroundImage = "url('images/fog.jpg')";
+                    document.getElementById("container").style.background = "#78777F";
+                }
 
-        // select city class and append the user location
-        let location = data.name;
-        $(".city").append(location + " Weather");
+                // select city class and append the user location
+                let location = data.name;
+                //sessionStorage.setItem("cityName", data.name);
+                $(".city").append(location + " Weather");
 
-        // Select the wind class and append the current wind speed
-        let wind = data.wind.speed;
-        $(".wind").append(wind + " mph");
+                // Select the wind class and append the current wind speed
+                let wind = data.wind.speed;
+                $(".wind").append(wind + " mph");
 
-        // Select the sunrise class
-        let rise = data.sys.sunrise;
-        var date = new Date(rise * 1000);
-        // Hours part from the timestamp
-        var hours = date.getHours();
-        // Minutes part from the timestamp
-        var minutes = "0" + date.getMinutes();
-        // Display time in 10:20 format
-        var formattedTime = hours + ":" + minutes.substr(-2);
-        // append the time the sun rose
-        $(".sunrise").append(formattedTime + " am");
+                // Select the sunrise class
+                let rise = data.sys.sunrise;
+                var date = new Date(rise * 1000);
+                // Hours part from the timestamp
+                var hours = date.getHours();
+                // Minutes part from the timestamp
+                var minutes = "0" + date.getMinutes();
+                // Display time in 10:20 format
+                var formattedTime = hours + ":" + minutes.substr(-2);
+                // append the time the sun rose
+                $(".sunrise").append(formattedTime + " am");
 
-        // Select the sunset class
-        let set = data.sys.sunset;
-        var date = new Date(set * 1000);
-        // Hours part from the timestamp
-        var hours = date.getHours();
+                // Select the sunset class
+                let set = data.sys.sunset;
+                var date = new Date(set * 1000);
+                // Hours part from the timestamp
+                var hours = date.getHours();
 
-        hours = hours % 12 || 12;
-        // Minutes part from the timestamp
-        var minutes = "0" + date.getMinutes();
-        // Display time in 10:20 format
-        var formattedTime = hours + ":" + minutes.substr(-2);
-        // append the time the sun rose
-        $(".sunset").append(formattedTime + " pm");
-      }
-    );
-  });
+                hours = hours % 12 || 12;
+                // Minutes part from the timestamp
+                var minutes = "0" + date.getMinutes();
+                // Display time in 10:20 format
+                var formattedTime = hours + ":" + minutes.substr(-2);
+                // append the time the sun rose
+                $(".sunset").append(formattedTime + " pm");
+            }
+        );
+    });
 
 //   Allows only one submission. One problem users have to refresh the page to submit again
-$("form").submit(function () {
-  $("input[type=submit]", this).attr("disabled", "disabled");
+$("form").submit(function() {
+    $("input[type=submit]", this).attr("disabled", "disabled");
 });
--- public/javascripts/landingPage.js
+++ public/javascripts/landingPage.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  //testAPICall();
+$(document).ready(function() {
+    //testAPICall();
 });
 
 //  API Testing
@@ -19,122 +19,122 @@ $(document).ready(function(){
 $("#detectLocation").click(submitDetectedLocation);
 
 function submitDetectedLocation() {
-  console.log("submit button");
-  getAutoLocation();
-  $(location).attr("href", "currentWeather.html");
+    console.log("submit button");
+    getAutoLocation();
+    $(location).attr("href", "currentWeather.html");
 }
 
 $("#zipCodeForm").submit(submitProvidedLocation);
 
 function submitProvidedLocation() {
-  let zipCode = $("#zipCode").val();
-  if(verifyZipCode(zipCode)){
-    setZipCode(zipCode);
-  } else{
-    setZipCode(null);
-    event.preventDefault();
-    alert("Sorry, it appears this zip code's weather data is unavailable.\nPlease enter a new zip code!");
-  }
+    let zipCode = $("#zipCode").val();
+    if (verifyZipCode(zipCode)) {
+        setZipCode(zipCode);
+    } else {
+        setZipCode(null);
+        event.preventDefault();
+        alert("Sorry, it appears this zip code's weather data is unavailable.\nPlease enter a new zip code!");
+    }
 }
 
 function getAutoLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(setCoords, showAutoLocError);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setCoords, showAutoLocError);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
 }
 
 function setCoords(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
-  if (typeof Storage !== "undefined") {
-    if (lat != null && lon != null) {
-      sessionStorage.setItem("lat", lat);
-      sessionStorage.setItem("lon", lon);
-      sessionStorage.setItem("zipCode", "undetected");
+    if (typeof Storage !== "undefined") {
+        if (lat != null && lon != null) {
+            sessionStorage.setItem("lat", lat);
+            sessionStorage.setItem("lon", lon);
+            sessionStorage.setItem("zipCode", "undetected");
 
-      //Used to Test Lat/Lon
-      let loc1 = sessionStorage.getItem("lat");
-      let loc2 = sessionStorage.getItem("lon");
-      alert("Lat: " + loc1 + " Lon: " + loc2);
+            //Used to Test Lat/Lon
+            let loc1 = sessionStorage.getItem("lat");
+            let loc2 = sessionStorage.getItem("lon");
+            alert("Lat: " + loc1 + " Lon: " + loc2);
+        }
+    } else {
+        alert("Sorry, your browser does not support Web Storage...");
     }
-  } else {
-    alert("Sorry, your browser does not support Web Storage...");
-  }
 }
 
 function showAutoLocError(error) {
-  switch (error.code) {
-    case error.PERMISSION_DENIED:
-      alert("User denied the request for Geolocation.");
-      break;
-    case error.POSITION_UNAVAILABLE:
-      alert("Location information is unavailable.");
-      break;
-    case error.TIMEOUT:
-      alert("The request to get user location timed out.");
-      break;
-    case error.UNKNOWN_ERROR:
-      alert("An unknown error occurred.");
-      break;
-  }
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
 }
 
 
 
 
 function setZipCode(zipCode) {
-  if (typeof Storage !== "undefined") {
-    sessionStorage.setItem("zipCode", zipCode);
-    if(zipCode != null){
-      setCoordsFromZip(zipCode);
+    if (typeof Storage !== "undefined") {
+        sessionStorage.setItem("zipCode", zipCode);
+        if (zipCode != null) {
+            setCoordsFromZip(zipCode);
+        }
+    } else {
+        alert("Sorry, your browser does not support Web Storage...");
     }
-  } else {
-    alert("Sorry, your browser does not support Web Storage...");
-  }
 }
 
 function verifyZipCode(zipCode) {
-  let isValid = false;
-  apiKey = 'c0c35b925bbddaaf1dca134adf31f13a';
+    let isValid = false;
+    apiKey = 'c0c35b925bbddaaf1dca134adf31f13a';
 
-  if (zipCode != null && zipCode >= 0) {
-    $.ajax({
-      method: 'GET',
-      url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`,
-      async: false,
-      success: function(data) {
-        console.log(data);
-        isValid = true;
-      }, 
-      error: function(xhr, status, error) {
-        var errorMessage = xhr.status + ': ' + xhr.statusText;
-        console.log("Error: " + errorMessage);
+    if (zipCode != null && zipCode >= 0) {
+        $.ajax({
+            method: 'GET',
+            url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`,
+            async: false,
+            success: function(data) {
+                console.log(data);
+                isValid = true;
+            },
+            error: function(xhr, status, error) {
+                var errorMessage = xhr.status + ': ' + xhr.statusText;
+                console.log("Error: " + errorMessage);
+                isValid = false;
+            }
+        });
+    } else {
         isValid = false;
-      }
-    });
-  } 
-  else{
-    isValid = false;
-  }
-  return isValid;
+    }
+    return isValid;
 }
 
-function setCoordsFromZip(zipCode){
-  if(zipCode != null){
-    $.ajax({
-      method: 'GET',
-      url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`,
-      success: function(data) {
-        sessionStorage.setItem("lat", data.coord.lat);
-        sessionStorage.setItem("lon", data.coord.lon);
-      },
-      error: function(xhr, status, error) {
-        var errorMessage = xhr.status + ': ' + xhr.statusText;
-        console.log("Error: " + errorMessage);
-      }
-    });
-  }
+function setCoordsFromZip(zipCode) {
+    if (zipCode != null) {
+        $.ajax({
+            method: 'GET',
+            url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`,
+            success: function(data) {
+                sessionStorage.setItem("lat", data.coord.lat);
+                sessionStorage.setItem("lon", data.coord.lon);
+                sessionStorage.setItem("cityName", data.name);
+            },
+            error: function(xhr, status, error) {
+                var errorMessage = xhr.status + ': ' + xhr.statusText;
+                console.log("Error: " + errorMessage);
+            }
+        });
+    }
 }
