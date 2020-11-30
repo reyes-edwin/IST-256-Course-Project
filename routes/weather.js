0a0c33890dfb17014d4b0a19eacc526ea30aa3d4
dronk6 <dkr5301@psu.edu>
2020-11-30 13:27:40 -0500

    Implemented local API

--- routes/weather.js
+++ routes/weather.js
@@ -3,13 +3,14 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next){
+router.get('/coords', function(req, res, next){
     const apiKey = 'c0c35b925bbddaaf1dca134adf31f13a';
     
-    let lat = sessionStorage.getItem("lat");
-    let lon = -77.860001;
-    axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=daily,minutely,alerts&units=imperial&appid=c0c35b925bbddaaf1dca134adf31f13a').then(function(response){
-        res.send(response.data.current);
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.lon}&appid=${apiKey}`).then((response) => {
+        res.send(response.data);
+    }).catch((err) =>
+    {
+        res.send(err);
     });
 
 });
@@ -18,12 +19,13 @@ router.get('/', function(req, res, next){
 router.get('/zipCode', function(req, res, next) {
     const apiKey = 'c0c35b925bbddaaf1dca134adf31f13a';
 
+    // , {timeout: 2}
+    
     axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${req.query.zipCode},us&appid=${apiKey}`).then((response) => { 
-        res.send(response.data.current);
+        res.send(response.data);
     }).catch((err) =>
     {
-        console.log('An error has occurred');
-        
+        res.send(err);
     });
     
     
